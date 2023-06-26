def4737a87f5ee7381923aa8e4ed0aa79f0d63d2
cioby09 <claudiociobanu09@gmail.com>
2023-06-26 19:28:48 +0200

    Milestone 4: Main layout and cards

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -27,12 +27,12 @@ export default {
         <h2 v-if="!card.poster_path">
             <img src="../assets/img/notavailable.jpg" alt="">
         </h2>
-        <ul>
+        <ul class="card-info d-flex">
             <li v-if="card.title">Titolo: {{ card.title }}</li>
             <li v-else>Nome:{{ card.name }}</li>
             <li>Titolo originale: {{ card.original_title ? card.original_title : card.original_name }}</li>
             <li>Lingua: <lang-flag :iso="`${card.original_language}`" /></li>
-            <li>Voto: <span v-for="i in store.stars">
+            <li>Voto: <span class="stars" v-for="i in store.stars">
                     <i :class="[(i <= setVote) ? 'fa-solid fa-star' : 'fa-regular fa-star']"></i>
                 </span>
             </li>
@@ -44,9 +44,34 @@ export default {
 <style scoped lang="scss">
 .card {
     height: 100%;
-}
+    background-color: black;
+    border: 2px solid white;
+    color: white;
+    border-radius: 0;
+    .card-info {
+        flex-direction: column;
+        position: absolute;
+        top: 0px;
+        right: 0px;
+        width: 100%;
+        padding: 30px 10px;
+        opacity: 0;
+        background-color: black;
+        height: 100%;
+        overflow-y: auto;
 
-img {
+        &:hover {   
+            opacity: 1;
+        }
+        li {
+        list-style: none;
+        }
+        .stars {
+            color: #FDCC0D;
+        }
+    }
+    img {
     width: 100%;
+    }
 }
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -17,13 +17,43 @@ export default {
 </script>
 
 <template>
-    <div class="row row-cols-6">
-        <div class="col" v-for="element in store.filmsArray">
-            <AppCard :card="element"/>
+    <main>
+        <div v-if="store.filmsArray.length === 0 & store.seriesArray.length === 0" class="container empty-home d-flex align-items-center justify-content-center">
+            <h1>TROVA IL TUO PROSSIMO SHOW</h1>
         </div>
-    </div>
+
+        <div v-else class="container">
+
+            <div class="container my-4">
+                <h2 class="text-white">Film</h2>
+                <div class="row row-cols-4 g-4">
+                    <div class="col" v-for="element in store.filmsArray">
+                        <AppCard :card="element"/>
+                    </div>
+                </div>
+            </div>
+            <div class="container my-4">
+                <h2 class="text-white">Serie TV</h2>
+                <div class="row row-cols-4 g-4">
+                    <div class="col" v-for="element in store.seriesArray">
+                        <AppCard :card="element"/>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </main>
 </template>
 
 <style scoped lang="scss" >
+.empty-home {
+    height: 100%;
+    color: 	black;
+}
 
+main {
+    background-color: 	#564d4d;
+    height: calc(100vh - 100px);
+    overflow: auto;
+}
 </style>
